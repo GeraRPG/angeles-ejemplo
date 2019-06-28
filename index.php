c62b1cfd368fce3cbf1bc4abebf8e8c33030fbde
Gerardo <gera@hubs.mx>
2019-06-28 09:07:07 -0500

    Se agrego el app

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include_once ('libs/app.php');
+include_once ('libs/model.php');
+include_once ('libs/controller.php');
+include_once ('libs/database.php');
+include_once ('libs/view.php');
+
+$app = new App();
